611b51ee5b0b5e93c317f11810f828dfa1f7e21c
ibrown <ibrown@twitter.com>
2017-06-25 14:41:21 -0700

    refactoring to parameterize screennames

--- src/ianisqatsibot.erl
+++ src/ianisqatsibot.erl
@@ -1,5 +1,11 @@
 -module(ianisqatsibot).
--export([get_alt_texts/0,get_previous_tweets/0,init/0,get_tweet_texts/1,run/0,run/1,bootstrap/1,decode_from_file/1,encode_to_file/2,bootstrap_loop/1]).
+-export([get_alt_texts/1,get_previous_tweets/1,init/0,get_tweet_texts/1,run/2,run/3,bootstrap/1,decode_from_file/1,encode_to_file/2,bootstrap_loop/3]).
+
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
 
 
 init()->
@@ -7,12 +13,12 @@ init()->
 		      application:start(X) end,
 	      [inets, crypto, public_key, ssl]).
 
-
-bootstrap_loop(FileName) ->
+%LOOP OFF OF PREVIOUSLY DOWNLOADED ALT TEXTS...NEEDED TO GET AROUND LIMIT OF 3000 TWEETS AND API LIMITS
+bootstrap_loop(AltTextSourceScreenName, BotScreenName, FileName) ->
     Alts=decode_from_file(FileName),
-    run(Alts),
-    timer:sleep(1000 * 60 * 60 * 6),
-    bootstrap_loop(FileName).
+    run(AltTextSourceScreenName, BotScreenName, Alts),
+    timer:sleep(1000 * 60 * 60 * 6), %DEFAULT INTERVAL OF ~6 HOURS...TODO: BASE THIS OFF OF LAST TWEET TIMESTAMP 
+    bootstrap_loop(AltTextSourceScreenName, BotScreenName,FileName).
 
 bootstrap(FileName) ->
     {ok, Device} = file:open(FileName, [read]),
@@ -82,12 +88,12 @@ read_lines(Device, Acc)->
          
 
 
-run()->
-    AltTexts=lists:map(fun(X)-> [Y]=X, Y end, get_alt_texts()),
-    run(AltTexts).
+run(AltTextSourceScreenName, BotScreenName)->
+    AltTexts=lists:map(fun(X)-> [Y]=X, Y end, get_alt_texts(AltTextSourceScreenName)),
+    run(AltTextSourceScreenName, BotScreenName,AltTexts).
 
-run(AltTexts)->
-    PreviousTweets = get_previous_tweets(),
+run(AltTextSourceScreenName, BotScreenName, AltTexts)->
+    PreviousTweets = get_previous_tweets(BotScreenName),
   
     PreviousTweetsSet = sets:from_list(get_tweet_texts(PreviousTweets)),
   
@@ -104,9 +110,9 @@ run(AltTexts)->
     erlybird:post(TweetBody, Consumer, AccessToken, AccessSecret).
 
 
-get_previous_tweets()->
+get_previous_tweets(ScreenName)->
     {Consumer, AccessToken, AccessSecret}=erlybird:get_secrets(),
-    Timeline = erlybird:get_entire_timeline([{count, "200"},{include_ext_alt_text, "true"}, {screen_name, "ianisqatsibot"}, {include_rts, "false"}], Consumer, AccessToken, AccessSecret),
+    Timeline = erlybird:get_entire_timeline([{count, "200"},{include_ext_alt_text, "true"}, {screen_name, ScreenName}, {include_rts, "false"}], Consumer, AccessToken, AccessSecret),
     Timeline.
 
 get_tweet_texts(Tweets)->
@@ -139,10 +145,10 @@ get_alt_text(Tweet)->
 		
     end.
 
-get_alt_texts()->
+get_alt_texts(ScreenName)->
     {Consumer, AccessToken, AccessSecret}=erlybird:get_secrets(),
     
-    Timeline = erlybird:get_entire_timeline([{count, "200"},{include_ext_alt_text, "true"}, {screen_name, "igb"}, {include_rts, "false"}], Consumer, AccessToken, AccessSecret, 15000),
+    Timeline = erlybird:get_entire_timeline([{count, "200"},{include_ext_alt_text, "true"}, {screen_name, ScreenName}, {include_rts, "false"}], Consumer, AccessToken, AccessSecret, 15000),
     AltTexts = lists:map(fun(X)->get_alt_text(X) end, Timeline),
     FilteredAltTexts = lists:filter(fun(X) -> case X of
 							       false ->
@@ -159,3 +165,21 @@ get_alt_texts()->
 		 end, FilteredAltTexts)).
     
     
+
+
+
+-ifdef(TEST).
+
+unicode_test() ->
+    Tweet=[65,32,99,114,111,112,112,101,100,32,118,105,101,119,32,111,102,32,97,32,83,
+ 97,109,115,117,110,103,226,132,162,32,109,111,110,105,116,111,114,46,32,84,
+ 104,101,32,118,105,115,105,98,108,101,32,112,111,114,116,105,111,110,32,111,
+ 102,32,116,104,101,32,115,99,114,101,101,110,32,100,105,115,112,108,97,121,
+ 115,32,116,104,101,32,87,105,110,100,111,119,115,194,169,32,55,32,80,114,111,
+	   102,101,115,115,105,111,110,97,108,32,108,111,103,111,46],
+
+    io:format("~s~n", [Tweet]),
+    ?assert(false  =:= true).
+
+
+-endif.
